8d3632fb2e9562cd6ce3e046d7952eed2cf50742
aman1468 <82465725+aman1468@users.noreply.github.com>
2026-02-07 22:51:03 -0600

    Added sql query for churn by contract

--- sql/02_churn_by_conract.sql
+++ sql/02_churn_by_conract.sql
@@ -1,1 +1,4 @@
-
+SELECT Contract, COUNT(*) AS total_customers,
+SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+ROUND( SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*),2) AS churn_rate_percent
+FROM "Telco-Customer-Churn" GROUP BY Contract;

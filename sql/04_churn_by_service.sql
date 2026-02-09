8770806b2993030a8678a0a1d771b604fbcf73f0
aman1468 <82465725+aman1468@users.noreply.github.com>
2026-02-09 00:16:04 -0600

    Create 04_churn_by_service.sql

---
+++ sql/04_churn_by_service.sql
@@ -0,0 +1,4 @@
+SELECT InternetService, COUNT(*) AS total_customers,
+SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+ROUND( SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*),2) AS churn_rate_percent
+FROM "Telco-Customer-Churn" GROUP BY InternetService;
